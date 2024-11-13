f358e2cfe5c614763f7cfc6d4d7be01258cb54bc
David Agámez <davidagamez04@gmail.com>
2024-11-13 02:14:11 -0500

    feat: dto for pagination implemented

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,13 @@
+import { IsOptional, IsPositive, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class PaginationDto {
+  @IsOptional()
+  @IsPositive()
+  @Type(() => Number)
+  limit?: number;
+  @IsOptional()
+  @Type(() => Number)
+  @Min(0)
+  offset?: number;
+}
