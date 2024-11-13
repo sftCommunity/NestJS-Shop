b7eab56c4f7bc67b1ef85e7e8f5af270c13c3303
David Agámez <davidagamez04@gmail.com>
2024-11-13 00:59:17 -0500

    feat: nestjs connected with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST_DB,
+      port: +process.env.PORT_DB,
+      database: process.env.NAME_DB,
+      username: process.env.USERNAME_DB,
+      password: process.env.PASSWORD_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
