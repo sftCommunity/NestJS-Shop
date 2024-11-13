ea7304eb4682fc55c0a95356e2095b1e7ea178ff
David Agámez <davidagamez04@gmail.com>
2024-11-13 02:19:22 -0500

    fix: ';' expected in product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -12,7 +12,7 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PaginationDto } from"../common/dto/pagination.dto"';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Controller('products')
 export class ProductsController {
