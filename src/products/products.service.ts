4524b658b44b5e970c9e88dfe607f3f78a75dcbd
David Agámez <davidagamez04@gmail.com>
2024-11-13 15:10:03 -0500

    feat: method for delete all products with his relations

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -131,4 +131,13 @@ export class ProductsService {
       'Unexpected error, check server logs',
     );
   }
+
+  async deleteAllProducts() {
+    const query = this.productRepository.createQueryBuilder('product');
+    try {
+      return await query.delete().where({}).execute();
+    } catch (e) {
+      this.handleDatabaseExceptions(e);
+    }
+  }
 }
