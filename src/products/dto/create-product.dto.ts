425ca619f8bb2885ba05e60fec3f9c58a7b6a163
David Agámez <davidagamez04@gmail.com>
2024-11-13 02:58:09 -0500

    update(product): adding tags column to products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -13,23 +13,34 @@ export class CreateProductDto {
   @IsString()
   @MinLength(1)
   title: string;
+
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
+
   @IsString()
   @IsOptional()
   description?: string;
+
   @IsString()
   @IsOptional()
   slug?: string;
+
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
+
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
+
   @IsIn(['men', 'women', 'kids', 'unisex'])
   gender: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  tags: string[];
 }
