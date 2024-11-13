67f6646b0d0baa1f1b88c85f8b36984251fe4e17
David Agámez <davidagamez04@gmail.com>
2024-11-13 01:23:01 -0500

    update: dto validations for create product

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,35 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+  @IsString()
+  description?: string;
+  @IsPositive()
+  @IsString()
+  @IsPositive()
+  slug?: string;
+  @IsInt()
+  @IsPositive()
+  @IsPositive()
+  stock?: number;
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+  @IsIn(['men', 'women', 'kids', 'unisex'])
+  gender: string;
+}
