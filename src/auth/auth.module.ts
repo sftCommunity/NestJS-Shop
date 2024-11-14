7dda4158e1eaaf69616c354302e71022eb5775a8
David Agámez <davidagamez04@gmail.com>
2024-11-13 19:52:13 -0500

    feat: jwt strategy configured

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,11 +6,13 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
@@ -23,6 +25,6 @@ import { User } from './entities/user.entity';
       },
     }),
   ],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
